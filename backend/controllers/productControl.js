88f06f94a3abb4e92cda9f9bcd358c898a3b578d
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-03-30 21:39:43 +0100

    changes to add product endpoint

--- backend/controllers/productControl.js
+++ backend/controllers/productControl.js
@@ -1,20 +1,19 @@
-const product = require("../models/product.model");
-const { createProductService } = require("../services/productService");
+const product = require('../models/product.model');
+const { createProductService } = require('../services/productService');
 
-const createProduct = async (req, res)=>{
-    const {ProductName} = req.body;
-    try {
-        if(ProductName){
-            const result = await createProductService({ProductName})
-            if(result){
-                res.status(200).json({message: 'product created'})
-            }
-        }
-    } catch (error) {
-        const errors = handleErrors(error);
-        res.status(400).json({errors});
+const createProduct = async (req, res) => {
+  const { ProductName } = req.body;
+  try {
+    if (ProductName) {
+      const result = await createProductService({ ProductName });
+      if (result) {
+        res.status(200).json({ message: 'product created', result });
+      }
     }
-}
+  } catch (error) {
+    const errors = handleErrors(error);
+    res.status(400).json({ errors });
+  }
+};
 
-
-module.exports = {createProduct}
+module.exports = { createProduct };
