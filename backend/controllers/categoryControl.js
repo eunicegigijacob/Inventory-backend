0adbb3d191e7887de50b21ddafd9a3de8e3b6aae
IfeoluwaOkesola <99264083+IfeoluwaOkesola@users.noreply.github.com>
2023-03-30 20:50:35 +0100

    create category endpoint

--- backend/controllers/categoryControl.js
+++ backend/controllers/categoryControl.js
@@ -1,3 +1,19 @@
-const category = require("../models/category.model");
-const { createCategoryService } = require("../services/categoryService");
+const category = require('../models/category.model');
+const { createCategoryService } = require('../services/categoryService');
 
+const createCategory = async (req, res) => {
+  const { categoryName } = req.body;
+  try {
+    if (categoryName) {
+      const result = await createCategoryService({ categoryName });
+      if (result) {
+        res.status(200).json({ message: 'category created' });
+      }
+    }
+  } catch (error) {
+    //const errors = handleErrors(error);
+    res.status(400).json({ errors });
+  }
+};
+
+module.exports = { createCategory };
--- backend/routes/rts.js
+++ backend/routes/rts.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { createCategory } = require('../controllers/categoryControl');
 const { createProduct } = require('../controllers/productControl');
 const { registerUser, loginUser } = require('../controllers/userControl');
 
@@ -7,5 +8,6 @@ const routeManager = express.Router();
 routeManager.post('/auth/register', registerUser);
 routeManager.post('/auth/login', loginUser);
 routeManager.post('/addProduct', createProduct);
+routeManager.post('/addCategory', createCategory);
 
 module.exports = routeManager;
