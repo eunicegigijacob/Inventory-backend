c856b0cfe5d4e31a681034edcfc0e2698ccfff4b
IfeoluwaOkesola <99264083+IfeoluwaOkesola@users.noreply.github.com>
2023-03-31 03:21:42 +0100

    dashboard function

--- backend/controllers/dashboard.js
+++ backend/controllers/dashboard.js
@@ -1,19 +1,22 @@
-const { authMiddlewares } = require("../middleware/auth");
-const { findStockService } = require("../services/stockService");
-const { handleErrors } = require("../utils/errorHandler");
+const { authMiddlewares } = require('../middleware/auth');
+const { findStockService } = require('../services/stockService');
+const { handleErrors } = require('../utils/errorHandler');
 
-
-const inflowDashboard = async (req,res)=>{
-    try {
-        const result = await findStockService()
-    if(result){
-       console.log(result) 
-       res.status(200).json({message: result})
+const inflowDashboard = async (req, res) => {
+  try {
+    const result = await findStockService();
+    if (result) {
+      console.log(result);
+      res
+        .status(200)
+        .json({
+          productName: result.stockName,
+          category: result.category,
+          quantity: result.quantity,
+          dateStocked: result.createdAt,
+        });
     }
-    } catch (error) {
-        
-    }
-    
-}
+  } catch (error) {}
+};
 
-module.exports = {inflowDashboard}
+module.exports = { inflowDashboard };
